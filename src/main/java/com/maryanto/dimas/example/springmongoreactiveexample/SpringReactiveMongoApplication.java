b916004679df3407f1900c04ff01372f118ed713
dimasm93 <software.dimas_m@icloud.com>
2020-09-30 14:17:53 +0700

    enabled mongo applicationn

--- src/main/java/com/maryanto/dimas/example/springmongoreactiveexample/SpringReactiveMongoApplication.java
+++ src/main/java/com/maryanto/dimas/example/springmongoreactiveexample/SpringReactiveMongoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMongoReactiveExampleApplication {
+public class SpringReactiveMongoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMongoReactiveExampleApplication.class, args);
+		SpringApplication.run(SpringReactiveMongoApplication.class, args);
 	}
 
 }
