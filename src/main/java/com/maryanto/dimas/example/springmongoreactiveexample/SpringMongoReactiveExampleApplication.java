4e8a8faa98295258f0caeb99bd6a0f60ef32095f
dimasm93 <software.dimas_m@icloud.com>
2020-09-30 14:17:11 +0700

    init project springboot

---
+++ src/main/java/com/maryanto/dimas/example/springmongoreactiveexample/SpringMongoReactiveExampleApplication.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example.springmongoreactiveexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoReactiveExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoReactiveExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maryanto/dimas/example/springmongoreactiveexample/SpringMongoReactiveExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maryanto.dimas.example.springmongoreactiveexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoReactiveExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
